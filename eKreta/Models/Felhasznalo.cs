988fe94810939b4e2d7a58716ae75abc8f78f6ac
JacsoAron0803 <jacsoaron@gmail.com>
2026-05-11 08:43:54 +0200

    Add project files.

---
+++ eKreta/Models/Felhasznalo.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKreta.Models
+{
+    public class Felhasznalo
+    {
+        public Felhasznalo()
+        {
+        }
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string FelhasznaloNev { get; set; }
+        public string TeljesNev { get; set; }
+        public string Jelszo {  get; set; }
+        public int Szerepkor { get; set; }
+
+        public Felhasznalo(string felhasznaloNev, string teljesNev, string jelszo, int szerepkor)
+        {
+            FelhasznaloNev = felhasznaloNev;
+            TeljesNev = teljesNev;
+            Jelszo = jelszo;
+            Szerepkor = szerepkor;
+        }
+    }
+}
---
+++ eKreta/Models/Oszatly.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKreta.Models
+{
+    public class Oszatly
+    {
+        public Oszatly()
+        {
+        }
+
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string OsztalyNev { get; set; }
+        public int TeremId { get; set; }
+
+        public Oszatly(string osztalyNev, int teremId)
+        {
+            OsztalyNev = osztalyNev;
+            TeremId = teremId;
+        }
+    }
+}
